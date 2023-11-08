2b5eca5957ed90dd562006d96b3206a1539f010b
Will Cygan <wcygan.io@gmail.com>
2023-11-08 00:11:55 -0600

    implementation

---
+++ src/file_log.rs
@@ -0,0 +1,108 @@
+use std::sync::Arc;
+use std::time::{SystemTime, SystemTimeError};
+
+use async_trait::async_trait;
+use tokio::fs::{File, OpenOptions};
+use tokio::io::{self, AsyncReadExt, AsyncSeekExt, AsyncWriteExt};
+use tokio::sync::Mutex;
+
+use crate::{Bytes, Log, LogError, Offset, Record};
+
+/// Represents the file-based log.
+struct FileLog {
+    file: Arc<Mutex<File>>,
+}
+
+impl FileLog {
+    /// Creates a new FileLog, opening the log file for both reading and writing.
+    /// The log file will be created if it does not exist.
+    pub async fn new(path: &str) -> Result<Self, LogError> {
+        let file = OpenOptions::new()
+            .create(true)
+            .read(true)
+            .write(true)
+            .append(true)
+            .open(path)
+            .await
+            .map_err(LogError::IoError)?;
+
+        Ok(FileLog {
+            file: Arc::new(Mutex::new(file)),
+        })
+    }
+}
+
+#[async_trait]
+impl Log for FileLog {
+    async fn append(&self, entry: Bytes) -> Result<Offset, LogError> {
+        let mut file = self.file.lock().await;
+
+        // Get the current offset
+        let offset = file
+            .seek(io::SeekFrom::End(0))
+            .await
+            .map_err(LogError::IoError)?;
+
+        // Create a new record
+        let record = Record {
+            value_length: entry.len() as u64,
+            value: entry,
+            timestamp: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)?
+                .as_millis(),
+        };
+
+        // Write the record to the file
+        file.write_all(&bincode::serialize(&record)?)
+            .await
+            .map_err(LogError::IoError)?;
+
+        Ok(offset)
+    }
+
+    async fn read(&self, offset: Offset) -> Result<Bytes, LogError> {
+        let mut file = self.file.lock().await;
+
+        // Seek to the offset
+        file.seek(io::SeekFrom::Start(offset))
+            .await
+            .map_err(LogError::IoError)?;
+
+        let mut length_buf = [0u8; 8];
+        file.read_exact(&mut length_buf)
+            .await
+            .map_err(LogError::IoError)?;
+        let value_length = u64::from_be_bytes(length_buf);
+
+        let mut value_buf = vec![0u8; value_length as usize];
+        file.read_exact(&mut value_buf)
+            .await
+            .map_err(LogError::IoError)?;
+
+        let mut timestamp_buf = [0u8; 16];
+        file.read_exact(&mut timestamp_buf)
+            .await
+            .map_err(LogError::IoError)?;
+        let _timestamp = u128::from_be_bytes(timestamp_buf);
+
+        Ok(value_buf)
+    }
+}
+
+impl From<bincode::Error> for LogError {
+    fn from(error: bincode::Error) -> Self {
+        LogError::SerializationError(error)
+    }
+}
+
+impl From<SystemTimeError> for LogError {
+    fn from(error: SystemTimeError) -> Self {
+        LogError::TimeError(error)
+    }
+}
+
+impl From<io::Error> for LogError {
+    fn from(error: io::Error) -> Self {
+        LogError::IoError(error)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use async_trait::async_trait;
 
+use serde::{Deserialize, Serialize};
+
 mod file_log;
 
 /// The position of a record within a log, represented as an offset from the beginning of the log.
@@ -9,19 +11,16 @@ type Offset = u64;
 type Bytes = Vec<u8>;
 
 /// Represents a single record within a log, consisting of a key, a value, and a timestamp.
+#[derive(Serialize, Deserialize)]
 struct Record {
-    /// The length of the log entry, in bytes.
-    length: u64,
+    /// The length of the value of the log entry (in bytes).
+    value_length: u64,
 
-    /// The key of the log entry, which can be used for partitioning or identifying the record.
-    /// Stored as a byte vector to support binary key data.
-    key: Bytes,
-
-    /// The value of the log entry. The actual data payload as a byte vector, supporting binary content.
+    /// The value of the log entry.
     value: Bytes,
 
     /// The timestamp when the log entry was created, represented as a Unix timestamp in milliseconds.
-    timestamp: u64,
+    timestamp: u128,
 }
 
 /// An enumeration of possible errors that can occur during log operations.
@@ -29,6 +28,12 @@ struct Record {
 pub enum LogError {
     /// Represents an I/O error, typically encountered during file operations (read/write).
     IoError(std::io::Error),
+
+    /// Represents a system time error, typically encountered when converting between system time and Unix time.
+    TimeError(std::time::SystemTimeError),
+    
+    /// Represents a serialization error, typically encountered when serializing or deserializing a record.
+    SerializationError(bincode::Error),
 }
 
 #[async_trait]
